4d91cc62a32bb3362d6fae1e314b9b84ba3f54fc
vinitmodi15 <92848833+vinitmodi15@users.noreply.github.com>
2024-06-22 12:42:30 +0530

    Update csiAssignment3.js

--- csiAssignment3.js
+++ csiAssignment3.js
@@ -1,23 +1,4 @@
-// with callback
-
-function getdata(data,getNextData){
-     setTimeout(()=>{
-         console.log(`data ${data}`);
-         if(getNextData){
-              getNextData();
-         }
-         
-     },32000);
-}
-console.log("with callback");
-getdata(1,()=>{
-  getdata(2,()=>{
-    getdata(3);
-  })
-})
-
-
-// // promise
+// promise
 
 function getData(data,getNextData){
   return new Promise((resolve,reject)=>{
