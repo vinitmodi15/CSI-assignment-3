03e54c9d6e51d1296a63009da94d1c984932b360
vinitmodi15 <92848833+vinitmodi15@users.noreply.github.com>
2024-06-22 12:49:32 +0530

    Update csiAssignment3.js

--- csiAssignment3.js
+++ csiAssignment3.js
@@ -1,40 +1,62 @@
 // promise
-
-function getData(data,getNextData){
-  return new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-         console.log(`data:${data}`)
-         resolve("success");
-         if(getNextData){
-          getNextData();
-         }
-    },3000)
-  })
+function getData(data, getNextData) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(`data:${data}`);
+      if (data === 2) {
+        reject("Error: Data 2 cannot be processed.");
+      } else {
+        resolve("success");
+      }
+      if (getNextData) {
+        getNextData();
+      }
+    }, 3000);
+  });
 }
-console.log("promise");
-getData(1)
-  .then((res)=>{
-  console.log(res);
-  return getData(2);})
-  .then((res)=>{
-    console.log(res);
-    return getData(3);});
 
+console.log("Promise Example:");
+
+getData(1)
+  .then((res) => {
+    console.log(res);
+    return getData(2);
+  })
+  .then((res) => {
+    console.log(res); // This will not execute due to rejection of getData(2)
+    return getData(3);
+  })
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((error) => {
+    console.error("Error occurred:", error);
+  });
 
 // // async await
 
-function getData(data){
-  return new Promise((resolve,reject)=>{
-    setTimeout(()=>{
+function getData(data) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
       console.log(`data ${data}`);
-    },4000)
-  })
-}
-async function asyncfunction(){
-  console.log("with async await");
-  await getData(1);
-  await getData(2);
+      if (data === 2) {
+        reject("Error: Data 2 cannot be processed.");
+      } else {
+        resolve("success");
+      }
+    }, 4000);
+  });
 }
 
-asyncfunction();
+async function asyncFunction() {
+  console.log("Async/Await Example:");
+  try {
+    await getData(1);
+    await getData(2); // This will throw an error
+    await getData(3);
+  } catch (error) {
+    console.error("Error occurred:", error);
+  }
+}
 
+asyncFunction();
